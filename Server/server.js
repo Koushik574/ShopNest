3d55c235fbcec6a67288ba79b2f34fde148a5cc5
Sai Koushik <koushiksai574@gmail.com>
2024-05-30 16:32:02 +0530

    🚀Initial commit

--- Server/server.js
+++ Server/server.js
@@ -1,55 +1,14 @@
-const express = require('express');
-const { Pool } = require('pg');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const app = express();
-const port = 5000;
-
-require('dotenv').config();
-
-
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-
-
-app.use(bodyParser.json());
-app.use(cors());
-
-
-app.post('/api/contact', async (req, res) => {
-  const { username, mail, phnumber, message } = req.body;
-
-  try {
-    await pool.query('INSERT INTO contacts (username, mail, phnumber, message) VALUES ($1, $2, $3, $4)', [username, mail, phnumber, message]);
-    res.status(201).send('Contact saved');
-  } catch (error) {
-    console.error('Error saving contact:', error);
-    res.status(500).send('Server error');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
-
-
-
 // const express = require('express');
 // const { Pool } = require('pg');
 // const bodyParser = require('body-parser');
 // const cors = require('cors');
 
 // const app = express();
-// const port = process.env.PORT || 5000;
+// const port = 5000;
 
 // require('dotenv').config();
+
+
 
 // const pool = new Pool({
 //   user: process.env.DB_USER,
@@ -59,11 +18,10 @@ app.listen(port, () => {
 //   port: process.env.DB_PORT,
 // });
 
+
 // app.use(bodyParser.json());
-// app.use(cors({
-//   origin: 'https://shopnest-8qfn.onrender.com',
-//   optionsSuccessStatus: 200
-// }));
+// app.use(cors());
+
 
 // app.post('/api/contact', async (req, res) => {
 //   const { username, mail, phnumber, message } = req.body;
@@ -80,4 +38,46 @@ app.listen(port, () => {
 // app.listen(port, () => {
 //   console.log(`Server running at http://localhost:${port}/`);
 // });
+
+
+
+const express = require('express');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+app.use(bodyParser.json());
+app.use(cors({
+  origin: 'https://shopnest-8qfn.onrender.com',
+  optionsSuccessStatus: 200
+}));
+
+app.post('/api/contact', async (req, res) => {
+  const { username, mail, phnumber, message } = req.body;
+
+  try {
+    await pool.query('INSERT INTO contacts (username, mail, phnumber, message) VALUES ($1, $2, $3, $4)', [username, mail, phnumber, message]);
+    res.status(201).send('Contact saved');
+  } catch (error) {
+    console.error('Error saving contact:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
 
