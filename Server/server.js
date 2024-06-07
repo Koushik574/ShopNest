9ae17c01038b0ea65391bcbe35a65c7db7d901f0
Sai Koushik <koushiksai574@gmail.com>
2024-06-07 09:11:50 +0530

    🚀Initial commit

--- Server/server.js
+++ Server/server.js
@@ -51,6 +51,14 @@ const port = process.env.PORT || 5000;
 
 require('dotenv').config();
 
+console.log({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
